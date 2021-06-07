3de080d9f21dcc43067edec7baf8562783a4e73e
Chris Deuda <christopherdeuda@gmail.com>
2021-06-07 23:57:16 +0800

    Add display of public announcement

--- src/views/Announcements.vue
+++ src/views/Announcements.vue
@@ -1,17 +1,60 @@
 <template>
-  <div class="home">
-    <Announcements></Announcements>
+  <div class="announcements">
+    <div>
+      <b-table striped hover :items="Announcements" :fields="fields">
+        <!-- A virtual column -->
+
+      </b-table>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Announcements from '@/components/Announcements.vue'
-
+import { mapGetters, mapActions } from "vuex";
 export default {
-  name: 'Home',
-  components: {
-    Announcements
-  }
+  name: 'Announcements',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      announcements: [],
+      fields: [
+        /*'id',*/
+        'title',
+        'content',
+        'startDate',
+        'endDate',
+      ],
+    }
+  },
+  created() {
+    //this.fetchAnnouncements();
+    this.GetAnnouncements();
+  },
+  computed: {
+    ...mapGetters({Announcements: "StateAnnouncements", User: "StateUser"}),
+  },
+
+  methods: {
+    ...mapActions([ "GetAnnouncements", "DeleteAnnouncement"]),
+
+    getUpdateId: function(id){
+      return parseInt(id );
+    },
+
+    deleteAnnouncement: function (id) {
+      if(confirm('Delete', 'Are you sure?')){
+        let $result = this.DeleteAnnouncement(id);
+        console.log($result);
+      }
+    },
+  },
+
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
