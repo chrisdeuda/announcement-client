23504db9871da0fd412ebc09b8a3ac8df3d45566
Chris Deuda <christopherdeuda@gmail.com>
2021-06-07 16:46:33 +0800

    Add default login

---
+++ src/views/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="login">
+    <div>
+      <form @submit.prevent="submit">
+        <div>
+          <label for="username">Username:</label>
+          <input type="text" name="username" v-model="form.email" />
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" name="password" v-model="form.password" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <p v-if="showError" id="error">Email or Password is incorrect</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "Login",
+  components: {},
+  data() {
+    return {
+      form: {
+        email: "test@gmail.com",
+        password: "12345",
+      },
+      showError: false
+    };
+  },
+  methods: {
+    ...mapActions(["LogIn"]),
+    async submit() {
+      const User = new FormData();
+      User.append("email", this.form.email);
+      User.append("password", this.form.password);
+      try {
+        await this.LogIn(User);
+        this.$router.push("/announcements");
+        this.showError = false
+      } catch (error) {
+        this.showError = true
+      }
+    },
+  },
+};
+</script>
